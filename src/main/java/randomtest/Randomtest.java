65f2969f483de68ecafcac62b522eec393a64822
gdevecz <gdevecz@gmail.com>
2021-10-30 20:43:04 +0200

    Hát nem sikerült a rnd problémát reprodukálnom, hogy ugyan azokat az értékeket dobja

--- src/main/java/randomtest/Randomtest.java
+++ src/main/java/randomtest/Randomtest.java
@@ -1,16 +1,35 @@
 package randomtest;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Randomtest {
 
     public static void main(String[] args) {
 
+        Math.decrementExact(2);
+        int[] a = new int[10];
+        Random rnd = new Random(13);
+        System.out.println(rnd.nextInt(100));
+        System.out.println(rnd.nextInt(100));
+
+
+        "ererer".strip();
+        Random rnd2 = new Random(13);
+        System.out.println(rnd2.nextInt(100));
+        System.out.println(rnd2.nextInt(100));
+
+        System.out.println(rnd.nextInt(1000));
+        System.out.println(rnd2.nextInt(100));
+/*
         Random rnd1 = new Random();
-        Random rnd2 = new Random();
+         rnd2 = new Random();
         Random rnd3 = new Random();
 
-        int a = rnd1.nextInt(100);
+         a = rnd1.nextInt(100);
         int b = rnd2.nextInt(100);
         int c = rnd3.nextInt(100);
 
@@ -37,5 +56,7 @@ public class Randomtest {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+
+ */
     }
 }
