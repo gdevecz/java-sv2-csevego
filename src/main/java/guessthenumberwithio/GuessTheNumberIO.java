3645444f1a97ba0c8f9151bf467d0fb3d5fdbdb3
gdevecz <gdevecz@gmail.com>
2021-10-30 21:02:55 +0200

    Na, a scanner tesztelése utáni módosítás

--- src/main/java/guessthenumberwithio/GuessTheNumberIO.java
+++ src/main/java/guessthenumberwithio/GuessTheNumberIO.java
@@ -9,11 +9,15 @@ public class GuessTheNumberIO {
     //    Lehet az kiküszöbölné a nextInt() problémáját, de a Stream könnyen lehet független a Scanner példányától,
     //    ezt még kitesztelem.
     //    Igazából konstruktor sem kéne, mert a Scanner-t az attributum deklarációnál inicializálhatnám is.
-    private Scanner sc;
+    //    NA KITESZTELTEM, ha új Scanner páldány van, a sc1 után sc2 akkor:
+    //    sc1.nextInt(); után az sc2 már a sortörés után kezdett, vagyis jó, ha a metódusban példányosítok
 
-    public GuessTheNumberIO() {
-        sc = new Scanner(System.in);
-    }
+    //ez akkor nem kell, így a kobstructor as maradhat az auto
+    //private Scanner sc;
+
+//    public GuessTheNumberIO() {
+//        sc = new Scanner(System.in);
+//    }
 
     public void introducingTheGame() {
         System.out.println("I have thought of a number between 1 and one hundred.");
@@ -37,21 +41,21 @@ public class GuessTheNumberIO {
                 " than the number.");
     }
 
-    //    Csak a nextInt()után a streamben maradt \n miatt. Itt nem lenne gond, de a progam bővítésénél
-    //    nem tudni hogy gondot okozna-e.
-    //    Így is jó lenne:
-    //    int guess = sc.nextInt();
-    //    sc.nextLine();
-    //    return guess;
-    //    Én így szoktam meg "Integer.parseInt(sc.nextLine())", nincs utána probléma, elfelejtett nextline(),
-    //    és egyből mehet értéknek return-nak is. Max. megmondják, hogy szokjak le róla. :)
+//            Csak a nextInt()után a streamben maradt \n miatt. Itt nem lenne gond, de a progam bővítésénél
+//            nem tudni hogy gondot okozna-e.
+//            Így is jó lenne:
+//            int guess = sc.nextInt();
+//            sc.nextLine();
+//            return guess;
+//            Én így szoktam meg "Integer.parseInt(sc.nextLine())", nincs utána probléma, elfelejtett nextline(),
+//            és egyből mehet értéknek return-nak is. Max. megmondják, hogy szokjak le róla. :)
     //
     //    Itt reprezentálnám, hogy én szívem szerint a paraméter fejléce és a törzse között hagynék egy sort,
     //    lehet más nyelvi berögzültség teszi. Erre nem emlékszem hogy említették volna hogy lehet vagy sem.
     //    Az hogy a { hol van, nem mozgat, csak vizuálisan számomra valahogy jobb :), de tény hogy nyújtja a kódot,
     //    lehet el kéne felejtenem.
     public int askANumber(int numberOfGuesses) {
-
+        Scanner sc = new Scanner(System.in);
         System.out.print("Give your " + numberOfGuesses + ". guess: ");
         return Integer.parseInt(sc.nextLine());
     }
