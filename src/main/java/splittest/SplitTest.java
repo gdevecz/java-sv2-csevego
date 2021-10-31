d4194a4004e7d166a7d2a5e3d6cfb5d7b5fa7234
gdevecz <gdevecz@gmail.com>
2021-10-31 14:46:31 +0100

    A split(".") nem jó,

---
+++ src/main/java/splittest/SplitTest.java
@@ -0,0 +1,9 @@
+package splittest;
+
+public class SplitTest {
+
+    public static void main(String[] args) {
+        String[] s = "1995.11.23".split(".");
+        System.out.println(s.length);
+    }
+}
